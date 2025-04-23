58beb28e48a29eabf7140bd8b2df7b16ad8293f2
mattiadelmonaco <mattia.delmonaco@gmail.com>
2025-04-23 22:11:27 +0200

    add style

--- laravel-migration-seeder/resources/views/home.blade.php
+++ laravel-migration-seeder/resources/views/home.blade.php
@@ -10,13 +10,55 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Treni Laravel</title>
     @vite(['resources/sass/app.scss', "resources/js/app.js"])
+    {{-- GOOGLE FONTS --}}
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Share+Tech+Mono&display=swap" rel="stylesheet">
 </head>
 <body>
-    <h1>ciao</h1>
-    <ul>
-        @foreach ($trains as $train)
-            <li>{{$train["agency"]}}</li>
-        @endforeach
-    </ul>
+    <header>
+        <div class="container">
+            <h1>LaravelTrains</h1>
+        </div>
+    </header>
+
+    <main>
+        <div class="container">
+            <ul class="list-unstyled">
+                @foreach ($trains as $train)
+                    <li>
+                        <span>{{$train["departure_date"]}}</span>
+                        <span>{{$train["agency"]}}</span>
+                        <span>{{$train["train_code"]}}</span>
+                        <span>{{$train["departure_station"]}}</span>
+                        <span>{{$train["departure_time"]}}</span>
+                        <span>{{$train["arrival_station"]}}</span>
+                        <span>{{$train["arrival_time"]}}</span>
+                        <span>{{$train["total_carriages"]}}</span>
+                        
+                        @if ($train["on_time"] === 1)
+                        <span>IN ORARIO</span>
+                        @else 
+                        <span>IN RITARDO</span>
+                        @endif 
+
+                        @if ($train["if_cancelled"] === 0)
+                        <span>NON CANCELLATO</span>
+                        @else
+                        <span>CANCELLATO</span>
+                        @endif
+
+
+                    </li>
+                @endforeach
+            </ul>
+        </div>
+    </main>
+
+    <footer>
+        <div class="container">
+            <h5>Made with ♥ by Mattia</h5>
+        </div>
+    </footer>
 </body>
 </html>
