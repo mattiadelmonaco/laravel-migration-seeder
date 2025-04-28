c49f72af399b92fedfb111fb91b81c906de78070
mattiadelmonaco <mattia.delmonaco@gmail.com>
2025-04-28 11:45:20 +0200

    feat: add orderby

--- laravel-migration-seeder/app/Http/Controllers/PageController.php
+++ laravel-migration-seeder/app/Http/Controllers/PageController.php
@@ -9,7 +9,7 @@ class PageController extends Controller
 {
     public function index() {
         $today = date('Y-m-d');
-        $trains = Train::whereDate('departure_date', '>=', $today)->get();
+        $trains = Train::whereDate('departure_date', '>=', $today)->orderBy("departure_date", "asc")->get();
         return view("home", compact("trains"));
     }
 }
